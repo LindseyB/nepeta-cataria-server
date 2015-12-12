336358afeac426b2858e271873fdb76cd2127d20
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-12 11:34:07 -0500

    Render nothing?

--- server.rb
+++ server.rb
@@ -2,10 +2,13 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 class Score < ActiveRecord::Base
+	validates_presence_of :score
+	validates_presence_of :name
 end
 
 class Server < Sinatra::Base
 	post '/add/:name/score/:score/' do
-		# add to database
+		Score.create(name: params['name'], score: params['score'].to_i)
+		render :nothing
 	end
 end
