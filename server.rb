3e8e727759325b2d9ca5f75bffe639f060756b0c
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-12 11:15:50 -0500

    Starting to add DB stuff

--- server.rb
+++ server.rb
@@ -1,4 +1,7 @@
 class Server < Sinatra::Base
+	register Sinatra::ActiveRecordExtension
+	set :data, {adapter: 'sqlite3', database: 'scores.sqlite3' }
+
 	post '/add/:name/score/:score/' do
 		# add to database
 	end
